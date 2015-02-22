da5cba04b4b0595c3ccc32203c891609dffb26f7
Kelsey Martin <kmartin@hearst.com>
2015-02-22 10:25:23 -0500

    no message

---
+++ Lessons/Lesson13/Athlete 101/Athlete 101Tests/Athlete_101Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Athlete_101Tests.swift
+//  Athlete 101Tests
+//
+//  Created by Kelsey Martin on 2/18/15.
+//  Copyright (c) 2015 Kelsey Martin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Athlete_101Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
